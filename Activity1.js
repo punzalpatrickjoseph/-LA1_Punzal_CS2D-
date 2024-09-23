1db52d1fb2e3f38af372143bbf3d43f7805f3f47
punzalpatrickjoseph <punzalpatrickjoseph17@gmail.com>
2024-09-23 09:54:17 +0800

    Activity1.js

---
+++ Activity1.js
@@ -0,0 +1,17 @@
+// Step 1: Create a variable named name and store your name
+let name = "PJ";
+
+// Step 2: Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer"
+let whatDoYouWannaBecomeInYourLife = "programmer";
+
+// Step 3: Create a variable named gender and store your gender
+let gender = "Male";
+
+// Step 4: Create a variable named twitterHandle and store your Twitter handle
+let twitterHandle = "N/A";
+
+// Step 5: Log all the variables to the console
+console.log("Name:", name);
+console.log("What do you wanna become in your life?", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
